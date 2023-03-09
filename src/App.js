beb021503ad5dacde3750442e8b890e1dbc3e208
Anthony <anthony.gyselman@gmail.com>
2023-03-09 12:48:56 +0100

    pushbeforedesign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import React, { useState } from "react";
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 //Importing Components
-import Form from './components/Form';
-import TodoList from './components/TodoList';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  // State
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // Run Once when the app start
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  // Use Effect
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  // Functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+  // Save to Local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+      };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      if (todoLocal.length === 0){
+        return null;
+      }
+      setTodos(todoLocal);
+    }
+  };
   return (
     <div className="App">
       <header>
         <h1>Nivus Todo List</h1>
       </header>
-      <Form inputText={inputText}
-            todos={todos}
-            setTodos={setTodos}
-            setInputText={setInputText}
-       />
-       
-      <TodoList  />
+      <div className="tdl-container">
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+
+      <TodoList filteredTodos={filteredTodos}
+                setTodos={setTodos}
+                todos={todos}
+      />
+      </div>
     </div>
   );
 }
